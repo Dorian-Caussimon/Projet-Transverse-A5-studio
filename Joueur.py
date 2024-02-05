120468b169a2ed5c5fde5f3326095467e925916e
Dorian caussimon <Dorian.caussimon.lucas@gmail.com>
2024-02-05 10:23:29 +0100

    start

--- Joueur.py
+++ Joueur.py
@@ -5,3 +5,5 @@ class joueur(pygame.sprite.Sprite):
         self.pv_max = 100
         self.image = pygame.image.load('Asset/Joueur/peso provi.png') # metre skin
         self.rect = self.image.get_rect()
+        self.rect.x = 10
+        self.rect.y = 500
--- Projectile.py
+++ Projectile.py
@@ -1,4 +1,11 @@
 import pygame
+
 class projectile(pygame.sprite.Sprite):
     def __init__(self):
-        self.test
+        self.velocity = 5
+        self.damage = 50
+        self.image = pygame.image.load("Asset/Projectile/projectile provisoire.png") #en attente
+        self.rect = self.image.get_rect()
+
+
+
