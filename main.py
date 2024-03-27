8be1d2339e01798d32abacb40e97b4988b84f703
Dorian Caussimon <Dorian.caussimon--lucas@efrei.net>
2024-03-27 20:02:28 +0100

    pos bouton ok

--- main.py
+++ main.py
@@ -8,26 +8,28 @@ pygame.display.set_caption("Manifeste") #nom de la fenètre
 screen = pygame.display.set_mode((800, 800)) # dimmension de la fenètre
 
 running = True
-jeu = jeux()
+je = jeux()
 
 clock = pygame.time.Clock() # charge le temps pour l'utiliser dans le jeux
 background = pygame.image.load('Asset/Background.png') #charge l'image de l'arrière plan
 background = pygame.transform.scale(background, (800,800))
+
 projectiles = pygame.sprite.Group()  # Groupe pour stocker les projectiles
 
-launch_angle = jeu.joueur.angle  # Angle initial
-launch_speed = jeu.joueur.speed  # Vitesse initiale
+launch_angle = je.joueur.angle  # Angle initial
+launch_speed = je.joueur.speed  # Vitesse initiale
 
 while running:
     time = clock.tick(60) / 1000 # Definit le temps
     screen.blit(background, (0, 0))
 
-    screen.blit(jeu.joueur.image, jeu.joueur.rect)
-    screen.blit(jeu.ennemi.image, jeu.ennemi.rect)
+    screen.blit(je.joueur.image, je.joueur.rect) # affiche le joueur
+    screen.blit(je.ennemi.image, je.ennemi.rect) # affiche les énemie
+
 
 
     # Dessiner la ligne d'indicateur de lancement
-    start_pos = jeu.joueur.rect.center
+    start_pos = je.joueur.rect.center
     end_pos = (start_pos[0] + int(math.cos(launch_angle) * launch_speed * 10), start_pos[1] - int(math.sin(launch_angle) * launch_speed * 10))
     pygame.draw.line(screen, (255, 0, 0), start_pos, end_pos, 2)
 
@@ -46,7 +48,7 @@ while running:
             if event.key == pygame.K_SPACE:
                 # Lancer un nouveau projectile
                 new_projectile = Projectile() # Crée une abréviation pour les nouveaux projectile
-                new_projectile.rect.center = jeu.joueur.rect.center
+                new_projectile.rect.center = je.joueur.rect.center
                 new_projectile.rect.centerx += int(math.cos(launch_angle) * 50)  # Ajuster la position du projectile en fonction de l'angle
                 new_projectile.rect.centery -= int(math.sin(launch_angle) * 50)  # Ajuster la position du projectile en fonction de l'angle
                 new_projectile.velocity_x = math.cos(launch_angle) * launch_speed # Ajuster la vitesse du projectile
