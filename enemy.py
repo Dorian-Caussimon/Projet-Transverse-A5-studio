097dd7074c88547de0e92c4ef63ca14d3c6f3866
elodie <elodie.fong@efrei.net>
2024-05-02 14:13:14 +0200

    ennemis sprites

--- enemy.py
+++ enemy.py
@@ -5,7 +5,7 @@ class ENEMY(pygame.sprite.Sprite):
         super().__init__()
         self.pv_max = random.choice((2,4))
         self.pv = self.pv_max
-        self.image = pygame.image.load('Asset/Joueur/peso provi.png') # metre skin
+        self.image = pygame.image.load('Asset/ennemis/peso provi.png') # metre skin
         self.rect = self.image.get_rect()
         self.rect.x = 900 + random.randint(0,200)
         self.rect.y = 500
